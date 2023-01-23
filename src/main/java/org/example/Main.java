112a7f61932cab1e473448eebde4ecb950583790
Angelo Paolella <ap2099903@gmail.com>
2023-01-22 23:50:11 -0800

    Initial commit

---
+++ src/main/java/org/example/Main.java
@@ -0,0 +1,26 @@
+package org.example;
+
+import org.javacord.api.DiscordApi;
+import org.javacord.api.DiscordApiBuilder;
+import org.javacord.api.interaction.SlashCommand;
+
+
+public class Main {
+    public static void main(String[] args) {
+
+        // bot token used for telling the bot the account to use
+        String token = "MTA2Njk2OTQzODU0MjM2NDcwMg.GbXLjD.QgipQNk0FmitJ_tWOgHM0wZzNOwnZNXfaZAvdw";
+        // the string that creates the bot and logs it in
+        DiscordApi api = new DiscordApiBuilder().setToken(token).setAllIntents().login().join();
+        //commands TODO: figure out why this command is not listing/working
+        SlashCommand test = SlashCommand.with("ping","checks if the bot is working properly").createGlobal(api).join();
+
+        // old way to make commands only here for testing as slash commands do not seem to be working
+        api.addMessageCreateListener(event -> {
+            if (event.getMessageContent().equalsIgnoreCase("!ping")){
+                event.getChannel().sendMessage("The bot works the slash command does not");
+            }
+        });
+
+    }
+}
