bbf542f7ba98c8bb935c6a500aea041446fe5552
Angelo Paolella <ap2099903@gmail.com>
2023-02-28 10:16:44 -0500

    added all the diffrent classes of commands to the application

---
+++ src/main/java/org/example/Roll_Channel_Management.java
@@ -0,0 +1,5 @@
+package org.example;
+
+public class Roll_Channel_Management {
+    // roll and channel management commands
+}
---
+++ src/main/java/org/example/Soundboard.java
@@ -0,0 +1,5 @@
+package org.example;
+
+public class Soundboard {
+    // soundboard bot  commands and implementations
+}
