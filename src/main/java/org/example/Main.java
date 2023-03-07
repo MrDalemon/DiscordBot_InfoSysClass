bb5cc8b0847df92a49060a8a71172786ca5493b5
Angelo Paolella <ap2099903@gmail.com>
2023-03-07 15:35:31 -0500

    tested the flipBool module and fixed it

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -3,10 +3,14 @@ package org.example;
 import org.javacord.api.DiscordApi;
 import org.javacord.api.DiscordApiBuilder;
 import org.javacord.api.interaction.SlashCommand;
+import org.javacord.api.interaction.SlashCommandInteraction;
+
+import java.util.concurrent.atomic.AtomicBoolean;
 
 
 public class Main {
     public static void main(String[] args) {
+        AtomicBoolean luckystatus = new AtomicBoolean(false);
 
         // bot token used for telling the bot the account to use
         Token toke = new Token();
@@ -14,14 +18,28 @@ public class Main {
         // the string that creates the bot and logs it in
         DiscordApi api = new DiscordApiBuilder().setToken(token).setAllIntents().login().join();
         //commands TODO: figure out why this command is not listing/working
-        SlashCommand test = SlashCommand.with("ping","checks if the bot is working properly").createGlobal(api).join();
+        SlashCommand test = SlashCommand.with("ping", "checks if the bot is working properly").createGlobal(api).join();
 
-        // old way to make commands only here for testing as slash commands do not seem to be working
-        api.addMessageCreateListener(event -> {
-            if (event.getMessageContent().equalsIgnoreCase("!ping")){
-                event.getChannel().sendMessage("The bot works the slash command does not");
+
+        api.addSlashCommandCreateListener(event -> {
+            SlashCommandInteraction interaction = event.getSlashCommandInteraction();
+            if (interaction.getFullCommandName().equals("ping")) {
+                luckystatus.set(flipBool(luckystatus.get()));
+                System.out.println(luckystatus.get());
+
+                // send message back
+                interaction.createImmediateResponder().setContent("the status of lucky is"+ luckystatus.get()).respond();
+
             }
         });
+    }
+
+// todo add the module switch commands and test if they work
+    public static boolean flipBool(boolean thing){
+
+        thing = !thing;
+        System.out.println("The value of the bool is: "+thing);
+        return thing;
 
     }
 }
