c6ae8f8c24ee5e2086dc564d4aad398c7e44e488
Angelo Paolella <ap2099903@gmail.com>
2023-04-15 15:43:08 -0400

    progress update

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -2,8 +2,11 @@ package org.example;
 
 import org.javacord.api.DiscordApi;
 import org.javacord.api.DiscordApiBuilder;
+import org.javacord.api.entity.permission.PermissionType;
 import org.javacord.api.interaction.SlashCommand;
 import org.javacord.api.interaction.SlashCommandInteraction;
+import org.javacord.api.interaction.SlashCommandOptionBuilder;
+import org.javacord.api.interaction.SlashCommandOptionChoice;
 
 import java.util.concurrent.atomic.AtomicBoolean;
 
@@ -20,26 +23,54 @@ public class Main {
         //commands TODO: figure out why this command is not listing/working
         SlashCommand test = SlashCommand.with("ping", "checks if the bot is working properly").createGlobal(api).join();
 
+        SlashCommand luckyStatus = SlashCommand.with("Lucky Status","sets the activity of the lucky module using a Boolean value Default : False ",
+                SlashCommandOptionChoice.create("option",)).setDefaultEnabledForPermissions(PermissionType.ADMINISTRATOR).createGlobal(api).join();
+
 
         api.addSlashCommandCreateListener(event -> {
             SlashCommandInteraction interaction = event.getSlashCommandInteraction();
             if (interaction.getFullCommandName().equals("ping")) {
-                luckystatus.set(flipBool(luckystatus.get()));
-                System.out.println(luckystatus.get());
+
+                // print to the console
+                System.out.println(interaction.getUser().getName().toString()+" used the command ping");
+                interaction.createImmediateResponder().setContent("this is full of shit ");
+
+            }
+        });
+
+        api.addSlashCommandCreateListener(event -> {
+            SlashCommandInteraction interaction = event.getSlashCommandInteraction();
+            if (interaction.getFullCommandName().equals("Lucky Status")) {
+
+                // print to the console
+                System.out.println(interaction.getUser().getName().toString()+" used the command ping");
+                // get the value for it and check if it is good
+                interaction.getArgumentAttachmentValueByName("option").isPresent();
+
+                if interaction.getar
+
 
                 // send message back
                 interaction.createImmediateResponder().setContent("the status of lucky is"+ luckystatus.get()).respond();
+                //todo add the module activation or deactivation events
+
 
             }
         });
     }
 
-// todo add the module switch commands and test if they work
-    public static boolean flipBool(boolean thing){
 
-        thing = !thing;
-        System.out.println("The value of the bool is: "+thing);
-        return thing;
+
+    }
+
+
+
+// todo add the module switch commands and test if they work
+    public static boolean flipBool(boolean boolToBeChanged){
+
+        boolToBeChanged = !boolToBeChanged;
+        System.out.println("The value of the bool is: "+boolToBeChanged);
+        return boolToBeChanged;
 
     }
 }
